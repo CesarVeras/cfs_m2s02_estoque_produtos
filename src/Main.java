056c572e13ce82504385bffad681d447607a4c55
Eduardo Cesar Veras dos Santos <eduardocvsantos@gmail.com>
2023-05-09 09:16:03 -0300

    Resolvendo os exercícios da semana 2

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import Entidades.Produtos;
+
+public class Main {
+    public static void main(String[] args) {
+        Produtos tvs = new Produtos("TV", 1300, 12);
+        System.out.printf("%s%n", tvs);
+        tvs.setQuantidade(20);
+        System.out.printf("%s%n", tvs);
+        tvs.setQuantidade(3);
+        System.out.printf("%s%n", tvs);
+    }
+}
+w
